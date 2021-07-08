7407db4471319f1b1eaa21be88d959f20cead48e
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-07-08 17:19:15 +0100

    Add files via upload

---
+++ openbox_custom/autostart.sh
@@ -0,0 +1,15 @@
+pulseaudio --start &
+nitrogen --restore &
+tint2 &
+compton &
+package-update-indicator &
+nm-applet &
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+kdeconnect-indicator &
+conky &
+plank &
+caffeine-indicator &
+xscreensaver -nosplash &
+xfce4-power-manager &
+mpd &
+sleep 2 & pnmixer 
