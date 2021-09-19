d535f87d69c6bf2ab29b192466398ec735abe701
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-09-19 13:28:46 +0100

    Update autostart.sh

--- openbox_custom/autostart.sh
+++ openbox_custom/autostart.sh
@@ -6,7 +6,6 @@ package-update-indicator &
 nm-applet &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 kdeconnect-indicator &
-conky &
 plank &
 caffeine-indicator &
 xscreensaver -nosplash &
