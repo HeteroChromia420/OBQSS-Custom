87d9d45c066d32755e749fbe4fe93220dd210e4c
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-12-21 16:56:58 +0000

    Update custom-min.sh

--- custom-min.sh
+++ custom-min.sh
@@ -11,8 +11,7 @@ mkdir ~/.config/openbox
 cp -r openbox_custom/* ~/.config/openbox
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install adriconf
-sudo apt-fast install -y openvpn dialog python3-pip python3-setuptools -y --no-install-recommends --no-install-suggests
-sudo pip3 install protonvpn-cli
+sudo apt-fast install -y openvpn network-manager-openvpn-gnome -y --no-install-recommends --no-install-suggests
 sudo apt update
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
 sudo apt purge --auto-remove tint2 cmst pnmixer dunst epiphany-browser
