4e273015e3335cbc5d8acdedf6946a0ed2de1aee
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-01-14 10:22:28 +0000

    Update custom-min.sh

--- custom-min.sh
+++ custom-min.sh
@@ -5,7 +5,7 @@ clear
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt-fast install xfce4-appmenu-plugin vala-panel-appmenu appmenu* firefox-esr ffmpegthumbnailer xfce4-pulseaudio-plugin xfce4-notifyd xfce4-panel xfce4-weather-plugin xfce4-whiskermenu-plugin desktop-base network-manager network-manager-gnome cantata gimp xscreensaver xserver-xorg-video-intel fonts-noto* arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme steam caffeine flatpak plank xfce4-power-manager -y --no-install-recommends --no-install-suggests 
+sudo apt-fast install xfce4-appmenu-plugin vala-panel-appmenu appmenu* firefox-esr ffmpegthumbnailer xfce4-notifyd xfce4-weather-plugin desktop-base network-manager network-manager-gnome cantata gimp xscreensaver xserver-xorg-video-intel fonts-noto* kdeconnect steam caffeine flatpak plank -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r openbox_custom/* ~/.config/openbox
@@ -14,15 +14,11 @@ sudo flatpak install adriconf
 sudo apt-fast install -y openvpn network-manager-openvpn-gnome -y --no-install-recommends --no-install-suggests
 sudo apt update
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
-sudo apt purge --auto-remove tint2 cmst pnmixer dunst epiphany-browser
+sudo apt purge --auto-remove epiphany-browser
 sudo cp 69-sysctl-performance.conf /etc/sysctl.d/
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
 sudo cp 52appmenu-gtk-module_add-to-gtk-modules /etc/X11/Xsession.d
-git clone https://github.com/dglava/arc-openbox
-mkdir
-cp -r arc-openbox/* ~/.themes
-rm -rf arc-openbox
 clear
 echo "don't forget to read zswap-help for details on improving swap support on your machine."
 read -n 1 -s -r -p "Press any key to continue..."
