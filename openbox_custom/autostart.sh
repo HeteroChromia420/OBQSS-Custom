add3eedff428d2ef68d86ac0e8b33de0fac73252
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-19 00:09:47 +0100

    Update autostart.sh

--- openbox_custom/autostart.sh
+++ openbox_custom/autostart.sh
@@ -1,7 +1,7 @@
 pulseaudio --start &
-pcmanfm --desktop &
-xfce4-panel &
-compton &
+nitrogen &
+tint2 &
+picom &
 package-update-indicator &
 nm-applet &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
