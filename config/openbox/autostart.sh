dd797206ae853f721f32f1304860193fe583921d
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-19 00:16:10 +0100

    Add files via upload

---
+++ config/openbox/autostart.sh
@@ -0,0 +1,13 @@
+pulseaudio --start &
+nitrogen &
+tint2 &
+picom &
+package-update-indicator &
+nm-applet &
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+kdeconnect-indicator &
+plank &
+caffeine-indicator &
+xscreensaver -nosplash &
+xfce4-power-manager &
+mpd &
--- custom.sh
+++ custom.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt-fast install ffmpegthumbnailer fonts-noto* kdeconnect steam caffeine flatpak plank zram-tools -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
-cp -r openbox_custom/* ~/.config/openbox
+cp -r config/* ~/.config/
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install adriconf
 sudo apt-fast install -y openvpn network-manager-openvpn-gnome -y --no-install-recommends --no-install-suggests
@@ -17,5 +17,4 @@ sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
 sudo apt purge --auto-remove epiphany-browser
 sudo cp 69-sysctl-performance.conf /etc/sysctl.d/
 clear
-echo "don't forget to read zswap-help for details on improving swap support on your machine."
 read -n 1 -s -r -p "Press any key to continue..."
