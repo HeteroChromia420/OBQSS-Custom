62f7ab3f2400066964083f4489c18e4046b51df0
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-28 17:10:52 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -5,7 +5,7 @@ clear
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install synaptic desktop-base gmrun xfce4-screenshooter caffeine xscreensaver xfce4-notifyd xfce4-power-manager package-update-indicator arc-theme breeze-cursor-theme picom wget ffmpegthumbnailer fonts-noto* kdeconnect steam caffeine flatpak plank zram-tools -y --no-install-recommends --no-install-suggests 
+sudo apt install synaptic xfce4-panel xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin firefox-esr desktop-base gmrun xfce4-screenshooter caffeine xscreensaver xfce4-notifyd xfce4-power-manager package-update-indicator arc-theme breeze-cursor-theme picom wget ffmpegthumbnailer fonts-noto* kdeconnect steam caffeine flatpak plank zram-tools -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r config/* ~/.config/
@@ -14,12 +14,8 @@ sudo flatpak install adriconf
 sudo apt install -y openvpn network-manager-openvpn-gnome -y --no-install-recommends --no-install-suggests
 sudo apt update
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
-sudo apt purge --auto-remove epiphany-browser dunst
+sudo apt purge --auto-remove epiphany-browser dunst tint2 pnmixer 
 sudo cp 69-sysctl-performance.conf /etc/sysctl.d/
-echo "deb [arch=amd64] http://deb.librewolf.net bullseye main" | sudo tee /etc/apt/sources.list.d/librewolf.list
-sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
-sudo apt update
-sudo apt install librewolf -y
 git clone https://github.com/dglava/arc-openbox
 cp -r arc-openbox/* ~/.themes
 rm -rf arc-openbox
