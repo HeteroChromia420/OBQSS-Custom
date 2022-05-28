f0b3f28620f582c83fcd0b1eb767c9ea883da266
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-28 17:11:35 +0100

    Update autostart.sh

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,7 +1,7 @@
 pulseaudio --start &
 nitrogen --restore &
-tint2 &
 picom &
+xfce4-panel &
 package-update-indicator &
 nm-applet &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
@@ -11,4 +11,3 @@ caffeine-indicator &
 xscreensaver -nosplash &
 xfce4-power-manager &
 mpd &
-pnmixer &
