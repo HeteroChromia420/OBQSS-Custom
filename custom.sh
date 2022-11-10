2807e7d6472531104cca9b34b188d04a26448460
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-11-10 15:58:26 +0000

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,7 +3,7 @@
 clear
 
 # in this sh file, there's just a bunch of packages that i personally use.
-sudo apt install xfce4-whiskermenu-plugin xfce4-appmenu-plugin vala-panel-appmenu appmenu* caffeine breeze-cursor-theme wget fonts-noto* kdeconnect plank zram-tools -y --no-install-recommends --no-install-suggests 
+sudo apt install dunst xfce4-whiskermenu-plugin xfce4-appmenu-plugin vala-panel-appmenu appmenu* caffeine breeze-cursor-theme wget fonts-noto* kdeconnect plank zram-tools -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r config/* ~/.config/
@@ -11,7 +11,7 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo apt install -y openvpn network-manager-openvpn-gnome -y --no-install-recommends --no-install-suggests
 sudo apt update
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
-sudo apt purge --auto-remove dunst tint2 pnmixer celluloid quodlibet gimp mpv youtube-dl epiphany-browser libreoffice 
+sudo apt purge --auto-remove tint2 pnmixer celluloid quodlibet gimp mpv youtube-dl epiphany-browser libreoffice 
 sudo cp 69-sysctl-performance.conf /etc/sysctl.d/
 wget -qO- https://git.io/papirus-folders-install | sh
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
