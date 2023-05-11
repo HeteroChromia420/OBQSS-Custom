3233adc149ea0d90e58c89858e57701202bdd89b
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-05-11 15:07:37 +0100

    Sync Repos

--- custom.sh
+++ custom.sh
@@ -3,7 +3,7 @@
 clear
 
 # in this sh file, there's just a bunch of packages that i personally use.
-sudo apt install wget unzip dunst xfce4-whiskermenu-plugin xfce4-appmenu-plugin vala-panel-appmenu appmenu* caffeine breeze-cursor-theme wget fonts-noto* kdeconnect plank zram-tools -y --no-install-recommends --no-install-suggests 
+sudo apt install wget unzip nitrogen dunst xfce4-whiskermenu-plugin xfce4-appmenu-plugin vala-panel-appmenu appmenu* caffeine breeze-cursor-theme wget fonts-noto* kdeconnect plank zram-tools -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r config/* ~/.config/
@@ -15,9 +15,10 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo apt install -y openvpn network-manager-openvpn-gnome -y --no-install-recommends --no-install-suggests
 sudo apt update
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
-sudo apt purge --auto-remove xfce4-notifyd tint2 pnmixer celluloid quodlibet gimp mpv youtube-dl epiphany-browser libreoffice 
+sudo apt purge --auto-remove xfce4-notifyd tint2 pnmixer celluloid quodlibet gimp mpv youtube-dl epiphany-browser libreoffice gnome-software
 sudo cp 69-sysctl-performance.conf /etc/sysctl.d/
 wget -qO- https://git.io/papirus-folders-install | sh
+sudo papirus-folders -C violet
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
 sudo cp 52appmenu-gtk-module_add-to-gtk-modules /etc/X11/Xsession.d
@@ -25,7 +26,7 @@ sudo cp -r wallpapers /home/
 wget https://github.com/dracula/gtk/archive/master.zip
 unzip master.zip
 mv gtk-master Dracula
-cp Dracula ~/.local/share/themes
+cp -r Dracula ~/.local/share/themes
 mv Dracula ~/.themes
 git clone https://github.com/dracula/openbox
 cp -r openbox/Dracula-withoutBorder/openbox-3 ~/.local/share/themes/Dracula/
